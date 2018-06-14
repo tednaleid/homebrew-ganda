a712156c4fb1d105bfbeac8d83bf159ff67becd9
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-13 22:41:52 -0500

    Brew formula update for ganda version v0.1.5

--- Formula/ganda.rb
+++ Formula/ganda.rb
@@ -1,9 +1,9 @@
 class Ganda < Formula
   desc "fast cmd-line app that quickly request millions of urls and save/echo the results"
   homepage "https://github.com/tednaleid/ganda"
-  url "https://github.com/tednaleid/ganda/releases/download/v0.1.4/ganda_Darwin_x86_64.tar.gz"
-  version "0.1.4"
-  sha256 "1abfc52e48bbf3a86fb316ce95f747a8f0982933bfb78e1c605ec53f39f6af61"
+  url "https://github.com/tednaleid/ganda/releases/download/v0.1.5/ganda_0.1.5_darwin_amd64.tar.gz"
+  version "0.1.5"
+  sha256 "5e5436b47b80b6955789a6797f55b1f4abe7d0d23d5242166c03df128dd8010d"
 
   def install
     bin.install "ganda"
