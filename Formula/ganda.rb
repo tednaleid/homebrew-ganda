7fdec62f3e3b18fd2c93e4f75d3d870a87d8e174
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-18 18:54:23 -0500

    Brew formula update for ganda version v0.1.8

--- Formula/ganda.rb
+++ Formula/ganda.rb
@@ -2,16 +2,16 @@
 class Ganda < Formula
   desc "fast cmd-line app that quickly request millions of urls and save/echo the results"
   homepage "https://github.com/tednaleid/ganda"
-  version "0.1.7"
+  version "0.1.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/tednaleid/ganda/releases/download/v0.1.7/ganda_0.1.7_darwin_amd64.tar.gz"
-    sha256 "333cc2dc4b3f61f88ee5158ee5cea9704ec6f8e4dfba572fdadccced9802d914"
+    url "https://github.com/tednaleid/ganda/releases/download/v0.1.8/ganda_0.1.8_darwin_amd64.tar.gz"
+    sha256 "424323d0b53107256b91a9623cb6b0cd0e000f544b091cfcb3925f34231f5054"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/tednaleid/ganda/releases/download/v0.1.7/ganda_0.1.7_linux_amd64.tar.gz"
-      sha256 "06f0e70e135ce6599c272be7364c92773f09751729e957ed48b8d3fc628a26fe"
+      url "https://github.com/tednaleid/ganda/releases/download/v0.1.8/ganda_0.1.8_linux_amd64.tar.gz"
+      sha256 "b5239f104a0900d12fb3de877022cf6f1543eeba24711424506d93bbdf20c7e3"
     end
   end
 
